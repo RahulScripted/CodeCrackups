f912ff7036461c6a36739df724c7113516b1d133
Rahul Goswami <goswami.rahul1002@gmail.com>
2025-04-08 10:17:48 +0530

    Deleted all

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender) => {
-  if (request.showMeme && sender.tab?.id) {
-    chrome.scripting.executeScript({
-      target: { tabId: sender.tab.id },
-      files: ["memeGenerator.js"],
-    });
-  }
-});
